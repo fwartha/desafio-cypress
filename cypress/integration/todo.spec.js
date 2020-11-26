5362686b1132585a18d2f7ae8b103c6aebb4a508
Lucas Amaral <lucas.amaral@handit.com.br>
2020-11-26 13:21:56 +0000

    alteração no todo.spec.js

--- cypress/integration/todo.spec.js
+++ cypress/integration/todo.spec.js
@@ -4,6 +4,12 @@ context('TODO LIST', () => {
       cy.visit('http://todomvc.com/examples/vanilla-es6/')
     })
   
+  // Criação de itens na todo list
+  // Exclusão de itens na todo list
+  // Marcar item como feito na todo list
+  // Testar a quantidade de itens que faltam ser completados (X items left)
+  // Testar os botões: All, Active, Completed, Clear Completed
+
     it('Caso de teste 1', () => {
 
     })
